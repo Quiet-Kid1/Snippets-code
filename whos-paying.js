3f60962ab37dcb226f3a292e8c989df355b0b690
E.S <62212426+E-S17@users.noreply.github.com>
2020-07-03 23:50:44 +0800

    Create whos-paying.js

---
+++ whos-paying.js
@@ -0,0 +1,8 @@
+function whosPaying(names) {
+
+names = ["Kaguya", "Chika", "Shirogane", "Ishigami", "Miko"];
+ 
+return names[Math.floor(Math.random() * names.length)] + " is going to buy lunch today!" ;
+    
+}
+whosPaying(); // 'random names' is going to buy lunch today!
